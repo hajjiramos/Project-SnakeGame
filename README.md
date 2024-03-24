62a49e7c380ac83e7bef160ddda31c16c46b2f09
Hajji Ramos <leo_pips@yahoo.com>
2024-03-24 15:35:27 -0500

    add playername, score and time

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Project-SnakeGame
 
 A snake game using basic HTML, CSS and JavaScript
+
+Disclaimer:
+snake background photo not mine
+owned by website: https://www.freepik.com/free-photos-vectors/snake-clipart
--- script.js
+++ script.js
@@ -1,205 +1,293 @@
-const gameBoard = document.querySelector('#gameBoard');
-const canvasContext = gameBoard.getContext('2d');
-const scoreDisplay = document.querySelector('#scoreDisplay');
-const btnPlay = document.querySelector('#btnPlay')
-const btnReset = document.querySelector('#btnReset');
-const gameWidth = gameBoard.width;
-const gameHeight = gameBoard.height;
-const unitSize = 25;
-let running = false;
-let xVelocity = unitSize;
-let yVelocity = 0;
+const canvas = document.querySelector('#canvas');
+const canvasContext = canvas.getContext('2d');
+const square = 25;
+let xAxis = square;
+let yAxis = 0;
 let foodXCoordinate;
 let foodYCoordinate;
-let score = 0;
 let snake = [
-    {xCoordinate:unitSize * 4, yCoordinate:0},
-    {xCoordinate:unitSize * 3, yCoordinate:0},
-    {xCoordinate:unitSize * 2, yCoordinate:0},
-    {xCoordinate:unitSize, yCoordinate:0},
-    {xCoordinate:0, yCoordinate:0},
+    {xCoordinate: 50, yCoordinate: 0},
+    {xCoordinate: 25, yCoordinate: 0},
+    {xCoordinate: 0, yCoordinate: 0}
 ]
-let speedDelay = 200;
+let move = false;
+let score = 0;
+let speed = 200;
 
-//-- Keyboard arrow keys function -- //
-window.addEventListener('keydown', changeDirection);
-btnPlay.addEventListener('click', gameStart)
-btnReset.addEventListener('click', resetGame);
+//--Function Game Front Page Display --//
 
+function frontPageDisplay(){
+    let img = document.getElementById('snake');
+    canvasContext.drawImage(img, 130, 10, 250, 480)
+    canvasContext.fillStyle = "black";
+    canvasContext.font = "10px san-serif";
+    canvasContext.fillText("https://www.freepik.com/free-photos-vectors/snake-clipart", 265, 498);
+    img.remove();
 
-
-function gameStart() {
-    running = true;
-    scoreDisplay.textContent = score;
-    createFood();
-    drawFood();
-    nextTick();
+    canvasContext.fillStyle = "darkgreen";
+    canvasContext.textAlign = "center";
+    canvasContext.font = "150px MyFault";
+    canvasContext.fillText("Snake", canvas.width/2, 230);
+    canvasContext.fillText("Game", canvas.width/2, 370);
+    
 }
+frontPageDisplay();
 
-function nextTick(){
-    if(running) {
-        setTimeout(()=>{
-            clearBoard();
-            drawFood();
-            moveSnake();
-            drawSnake();
-            checkGameOver();
-            nextTick();    
-        }, speedDelay);
+
+//-- Function Game Start and enter player name--//
+
+const scoreDisplay = document.querySelector('#scoreDisplay');
+const btnPlay = document.querySelector('#btnPlay');
+    document.querySelector('#btnTryAgain').disabled = true;
+    btnPlay.addEventListener('click', enterName); 
+
+
+function enterName() {
+    let name = prompt ("Please Enter your Name:");
+    const displayName = document.querySelector('.name');
+        
+    if (name != null && name !="") {
+        displayName.textContent = name;
+        game();  
     }
     else {
-        displayGameOver();
+        window.location.reload();
     }
 }
 
+function game() {
+    move = true;
+    scoreDisplay.textContent = score; 
+    document.querySelector('#btnTryAgain').disabled = false;
+    startTimer();
+    drawFood();
+    foodDisplay();
+    snakeMoving();
+    }
 
-function clearBoard(){
-    canvasContext.fillStyle = "whitesmoke";
-    canvasContext.fillRect(0, 0, gameWidth, gameHeight)
+
+//-- Function calling functions while snake is moving --//
+
+function snakeMoving(){
+    if(move) {
+        setTimeout(()=>{
+            clearCanvas();
+            foodDisplay();
+            moveSnake();
+            drawSnake();
+            gameOver();
+            snakeMoving();    
+        }, speed);
+    }
+    else {
+        gameOverDisplay();
+    }
+}
+
+//-- Function to clear canvas every game --//
+
+function clearCanvas(){
+    canvasContext.fillStyle = "lightgray";
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height)
 }
 
 
-function createFood(){
+//--Function to Draw Food in random locations--//
+function drawFood(){
     function randomFood(min, max){
-        const randNum = Math.round((Math.random() * (max - min) + min) / unitSize) * unitSize;
+        const randNum= Math.round((Math.random() * (max - min) + min) / square) * square;
         return randNum;
     }
-    foodXCoordinate = randomFood(0, gameWidth - unitSize);
-    foodYCoordinate = randomFood(0, gameWidth - unitSize);
+    foodXCoordinate = randomFood(0, canvas.width - square);
+    foodYCoordinate = randomFood(0, canvas.width - square);
 }
 
 
-function drawFood(){
+//-- Function to display the food --//
+
+function foodDisplay(){
     canvasContext.fillStyle = "red";
-    canvasContext.fillRect(foodXCoordinate, foodYCoordinate, unitSize, unitSize);
+    canvasContext.fillRect(foodXCoordinate, foodYCoordinate, square, square);
 }
 
-
+//-- Function to make the snake move and eat the food --//
 function moveSnake(){
-    const head = {xCoordinate: snake[0].xCoordinate + xVelocity,
-                  yCoordinate: snake[0].yCoordinate + yVelocity}
+    const head = {xCoordinate: snake[0].xCoordinate + xAxis,
+                  yCoordinate: snake[0].yCoordinate + yAxis}
 
     //adds box to the beginning of the Snake Array
     snake.unshift(head);
-
+   
     //check if food is eaten
     if (snake[0].xCoordinate == foodXCoordinate && snake[0].yCoordinate == foodYCoordinate){
-        score+=1;
-        speedDelay-=5;
-        scoreDisplay.textContent = score;
-        createFood();
+        score +=1;
+        speed -=3;
+        scoreDisplay.innerHTML = score;
+        drawFood();
     }  
     else {
         snake.pop();
     }
 }
 
-
+//-- Function to draw the snake starting at the firstCoordinate --//
 function drawSnake(){
-    canvasContext.fillStyle = "lightblue";
+    canvasContext.fillStyle = 'lightgreen';
     canvasContext.strokeStyle = "black";
     snake.forEach(snakeMove => {
-        canvasContext.fillRect(snakeMove.xCoordinate, snakeMove.yCoordinate, unitSize, unitSize)
-        canvasContext.strokeRect(snakeMove.xCoordinate, snakeMove.yCoordinate, unitSize, unitSize)
+        canvasContext.fillRect(snakeMove.xCoordinate, snakeMove.yCoordinate, square, square)
+        canvasContext.strokeRect(snakeMove.xCoordinate, snakeMove.yCoordinate, square, square)
     });
 }
 
 
-function changeDirection(event){
+//-- Function keypressing the Keyboard arrow keys -- //
+
+window.addEventListener('keydown', arrowKeys);
+function arrowKeys(event){
     const keyPressed = event.keyCode;
-    const arrowLeft = 37;
     const arrowUp = 38;
-    const arrowRight = 39;
     const arrowDown = 40;
-
-    const goingRight = (xVelocity == unitSize);
-    const goingLeft = (xVelocity == -unitSize);
-    const goingDown = (yVelocity == unitSize);
-    const goingUp = (yVelocity == -unitSize);
-
+    const arrowRight = 39;
+    const arrowLeft = 37;
+    
+    const goingUp = (yAxis == -square);
+    const goingDown = (yAxis == square);
+    const goingRight = (xAxis == square);
+    const goingLeft = (xAxis == -square);
+    
     //prevent pressing opposite directions
     switch(true){
         case (keyPressed == arrowLeft && !goingRight) :
-            xVelocity = -unitSize;
-            yVelocity = 0;
+            xAxis = -square;
+            yAxis = 0;
             break;
 
         case (keyPressed == arrowRight && !goingLeft) :
-            xVelocity = unitSize;
-            yVelocity = 0;
+            xAxis = square;
+            yAxis = 0;
             break;
         
         case (keyPressed == arrowUp && !goingDown) :
-            xVelocity = 0
-            yVelocity = -unitSize;
+            xAxis = 0
+            yAxis = -square;
             break;
 
         case (keyPressed == arrowDown && !goingUp) :
-            xVelocity = 0;
-            yVelocity = unitSize;
-            break;
-            
+            xAxis = 0;
+            yAxis = square;
+            break;    
     }
-
-
 
 }
 
 
-function checkGameOver(){
+//-- Function Game Over --//
+
+function gameOver(){
     switch(true){
         //touches the walls
         case (snake[0].xCoordinate < 0) :
-            running = false;
+            move = false;
             break;
 
-        case (snake[0].xCoordinate >= gameWidth) :
-            running = false;
+        case (snake[0].xCoordinate >= canvas.width) :
+            move = false;
             break;
 
         case (snake[0].yCoordinate < 0) :
-            running = false;
+            move = false;
             break;
 
-        case (snake[0].yCoordinate >= gameHeight) :
-            running = false;
+        case (snake[0].yCoordinate >= canvas.height) :
+            move = false;
             break;
     }
 
     //touches the body of the snake
     for (let i = 1; i < snake.length; i+=1){
         if (snake[i].xCoordinate == snake[0].xCoordinate && snake[i].yCoordinate == snake[0].yCoordinate){
-            running = false;
+            move = false;
         }     
     }
 }
 
 
-function displayGameOver(){
-    canvasContext.font = "50px DundalkHandDrawn";
-    canvasContext.fillStyle = "black";
+//--Function Display Game Over --//
+
+function gameOverDisplay(){
     canvasContext.textAlign = "center";
-    canvasContext.fillText("GAME OVER!", gameWidth/2, gameHeight/2);
-    running = false;
+    canvasContext.fillStyle = "blue";
+    canvasContext.font = "150px Hansa";
+    canvasContext.fillText("Game Over!", canvas.width/2, canvas.height/2);
     document.querySelector('#btnPlay').disabled=true;
+    move = false;
+    stopTimer();
 }
 
 
-function resetGame(){
+
+
+//-- Function Try Again Game --//
+
+const btnTryAgain = document.querySelector('#btnTryAgain');
+btnTryAgain.addEventListener('click', tryAgain);
+function tryAgain(){
     score = 0;
-    speedDelay = 200;
-    xVelocity = unitSize;
-    yVelocity = 0;
+    speed = 200;
+    xAxis = square;
+    yAxis = 0;
     snake = [
-        {xCoordinate:unitSize * 4, yCoordinate:0},
-        {xCoordinate:unitSize * 3, yCoordinate:0},
-        {xCoordinate:unitSize * 2, yCoordinate:0},
-        {xCoordinate:unitSize, yCoordinate:0},
-        {xCoordinate:0, yCoordinate:0},
+        {xCoordinate: 50, yCoordinate: 0},
+        {xCoordinate: 25, yCoordinate: 0},
+        {xCoordinate: 0, yCoordinate: 0}
     ];
-    gameStart();
+    game();
+    resetTimer();
 }
 
 
 
+//-- Functions Display Time Interval every seconds and minutes --//
 
+const timerDisplay = document.querySelector('#timerDisplay')
+let [minute, second] = [0,0];
+let time = null;
+scoreDisplay.innerHTML = 0;
+timerDisplay.innerHTML = "00:00";
+
+function timer() {
+    second++;
+    if (second == 60){
+        second = 0;
+        minute++;
+    }
+    let min = minute < 10 ? "0" + minute : minute;
+    let sec = second < 10 ? "0" + second : second;
+
+    timerDisplay.innerHTML = min +":"+ sec;
+}
+
+
+function startTimer() {
+    if (time !== null) {
+        clearInterval(time);
+    }
+    time = setInterval(timer,1000);
+}
+
+
+function stopTimer() {
+    clearInterval(time);
+}
+
+
+function resetTimer() {
+    clearInterval(time);
+    [minute, second] = [0,0];
+    timerDisplay.innerHTML = "00:00";
+    startTimer();
+}
+
+   
+ 
